dd75fe0ef00cdfb254e73a2a030d85dfc3ec2220
AiWorld <hongliang@cqteam.top>
2021-11-15 15:39:36 +0800

    [开发] - 使用DataStore

---
+++ library-base/src/main/java/com/cqteam/baselibrary/utils/DataStoreUtils.kt
@@ -0,0 +1,17 @@
+package com.cqteam.baselibrary.utils
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.preferencesDataStore
+import java.util.prefs.Preferences
+
+/**
+ * Author： 洪亮
+ * Time： 2021/11/3 - 4:24 PM
+ * Email：281332545@qq.com
+ * <p>
+ * 描述：
+ */
+object DataStoreUtils {
+
+
+}
